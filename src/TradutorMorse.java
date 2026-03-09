2dae3054fea92f4d6fed34579d59c9a974457c12
BernardoRafael1 <bernardorafael.achaves@gmail.com>
2026-03-09 17:20:09 -0300

    criando classes e imports

---
+++ src/TradutorMorse.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class TradutorMorse {
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        HashMap<String, String> mapaMorse = new HashMap<>();
+    }
+}
