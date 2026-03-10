df61f895f6b74e25f36869a57efb3ae87daaafdc
BernardoRafael1 <bernardorafael.achaves@gmail.com>
2026-03-10 15:31:30 -0300

    testando o mapeamento do alfabeto usando HashMap (A, B e espaço)

--- src/TradutorMorse.java
+++ src/TradutorMorse.java
@@ -7,5 +7,18 @@ public class TradutorMorse {
         Scanner leitor = new Scanner(System.in);
 
         HashMap<String, String> mapaMorse = new HashMap<>();
+
+        mapaMorse.put("A", ".-");
+        mapaMorse.put("B", "-...");
+        mapaMorse.put(" ", "/");
+
+        System.out.println("TRADUTOR DE CÓDIGO MORSE");
+        System.out.print("Digite o texto que queira traduzir: ");
+
+        String entrada = leitor.nextLine();
+
+        System.out.println("Você digitou: " + entrada);
+
+        leitor.close();
     }
 }
