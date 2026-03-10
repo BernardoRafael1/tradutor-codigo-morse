c449f8441975cb0e411a9970bf4a9f68934b65d5
BernardoRafael1 <bernardorafael.achaves@gmail.com>
2026-03-10 16:13:53 -0300

    mapeando todo o alfabeto para código morse

--- src/TradutorMorse.java
+++ src/TradutorMorse.java
@@ -10,6 +10,30 @@ public class TradutorMorse {
 
         mapaMorse.put("A", ".-");
         mapaMorse.put("B", "-...");
+        mapaMorse.put("C", "-.-.");
+        mapaMorse.put("D", "-..");
+        mapaMorse.put("E", ".");
+        mapaMorse.put("F", "..-.");
+        mapaMorse.put("G", "--.");
+        mapaMorse.put("H", "....");
+        mapaMorse.put("I", "..");
+        mapaMorse.put("J", ".---");
+        mapaMorse.put("K", "-.-");
+        mapaMorse.put("L", ".-..");
+        mapaMorse.put("M", "--");
+        mapaMorse.put("N", "-.");
+        mapaMorse.put("O", "---");
+        mapaMorse.put("P", ".--.");
+        mapaMorse.put("Q", "--.-");
+        mapaMorse.put("R", ".-.");
+        mapaMorse.put("S", "...");
+        mapaMorse.put("T", "-");
+        mapaMorse.put("U", "..-");
+        mapaMorse.put("V", "...-");
+        mapaMorse.put("W", ".--");
+        mapaMorse.put("X", "-..-");
+        mapaMorse.put("Y", "-.--");
+        mapaMorse.put("Z", "--..");
         mapaMorse.put(" ", "/");
 
         System.out.println("TRADUTOR DE CÓDIGO MORSE");
